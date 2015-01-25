fe2df7cd07609aaba18fb8fdb6a2daea92b4413c
rfnajid <ridhaaaaazis@gmail.com>
2015-01-25 11:15:13 +0700

    first commit

---
+++ Splash.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	public AnimationClip animation;
+	public string nextscene;
+
+	void Start () {
+		float time=animation.length;
+		StartCoroutine(WaitAndDo(time));
+	}
+
+	IEnumerator WaitAndDo(float time){
+		yield return new WaitForSeconds(time);
+		//do something after wait.
+		Do ();
+	}
+
+	void Do(){
+		Application.LoadLevel(nextscene);
+	}
+}
